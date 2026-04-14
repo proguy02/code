36df22063cc6ec41f8f865143d1ee30c03c3a315
rnichola26 <devraymond24@gmal.com>
2026-04-14 15:06:24 +0100

    repo

--- main.go
+++ main.go
@@ -3,9 +3,33 @@ package main
 import (
 	"code/utils"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println(utils.Base("Simply add 42 (hex) and 10 (bin) and you will see the result is 68."))
-	fmt.Println(utils.String("it (cap) was the best of times, it was the worst of times (up) , it was the age of wisdom, it was the age of foolishness (cap, 6) , it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of darkness, it was the spring of hope, IT WAS THE (low, 3) winter of despair."))
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: go run . input.txt output.txt")
+		return
+	}
+	input := os.Args[1]
+	output := os.Args[2]
+
+	text, err := os.ReadFile(input)
+	if err != nil {
+		fmt.Println("error reading file", err)
+		return
+	}
+	words := string(text)
+	words = utils.String(words)
+	words = utils.AnA(words)
+	//words = utils.Qoute(words)
+	//words = utils.Puncts(words)
+	words = utils.Base(words)
+
+	err = os.WriteFile(output, []byte(words), 0644)
+	if err != nil {
+		fmt.Println("error writing to file", err)
+		return
+	}
+	fmt.Println("file modified successfully.")
 }
--- utils/base_conv.go
+++ utils/base_conv.go
@@ -15,6 +15,7 @@ func Base(str string) string {
 				return err.Error()
 			}
 			word[r-1] = strconv.FormatInt(conv, 10)
+			word = append(word[:r], word[r+1:]...)
 		}
 		if word[r] == "(bin)" && r > 0 {
 			conv, err := strconv.ParseInt(word[r-1], 2, 64)
@@ -22,7 +23,94 @@ func Base(str string) string {
 				return err.Error()
 			}
 			word[r-1] = strconv.FormatInt(conv, 10)
+			word = append(word[:r], word[r+1:]...)
 		}
 	}
 	return strings.Join(word, " ")
+}
+
+func String(str string) string {
+	word := strings.Fields(str)
+
+	for r := 0; r < len(word); r++ {
+		if word[r] == "(up)" && r > 0 {
+			word[r-1] = strings.ToUpper(word[r-1])
+			word = append(word[:r], word[r+1:]...)
+		}
+		if word[r] == "(low)" && r > 0 {
+			word[r-1] = strings.ToLower(word[r-1])
+			word = append(word[:r], word[r+1:]...)
+		}
+		if word[r] == "(cap)" && r > 0 {
+			word[r-1] = strings.ToUpper(word[r-1][:1]) + strings.ToLower(word[r-1][1:])
+			word = append(word[:r], word[r+1:]...)
+		}
+		if word[r] == "(up," && r > 0 {
+			next := word[r+1]
+			next = strings.Trim(next, ")")
+			conv, _ := strconv.Atoi(next)
+			for j := r - conv; j < r; j++ {
+				word[j] = strings.ToUpper(word[j])
+			}
+			word = append(word[:r], word[r+2:]...)
+		}
+		if word[r] == "(low," && r > 0 {
+			next := word[r+1]
+			next = strings.Trim(next, ")")
+			conv, _ := strconv.Atoi(next)
+			for j := r - conv; j < r; j++ {
+				word[j] = strings.ToLower(word[j])
+			}
+			word = append(word[:r], word[r+2:]...)
+		}
+		if word[r] == "(cap," && r > 0 {
+			next := word[r+1]
+			next = strings.Trim(next, ")")
+			conv, _ := strconv.Atoi(next)
+			for j := r - conv; j < r; j++ {
+				word[j] = strings.ToUpper(word[j][:1]) + strings.ToUpper(word[j][1:])
+			}
+			word = append(word[:r], word[r+2:]...)
+		}
+	}
+	return strings.Join(word, " ")
+}
+
+func Qoute(sen string) string {
+	words := strings.Split(sen, "'")
+
+	for x := 1; x < len(words); x += 2 {
+		words[x] = strings.TrimSpace(words[x])
+	}
+	return strings.Join(words, "'")
+}
+
+func AnA(s string) string {
+	words := strings.Fields(s)
+
+	for x := 0; x < len(words); x++ {
+		if words[x] == "a" && strings.ContainsAny(words[x+1][:1], "aeiouhAEIOUH") {
+			words[x] = "an"
+		} else if words[x] == "A" && strings.ContainsAny(words[x+1][:1], "aeiouhAEIOUH") {
+			words[x] = "An"
+		}
+	}
+	return strings.Join(words, " ")
+}
+
+func Puncts(s string) string {
+	words := strings.Fields(s)
+	result := []string{}
+	for _, char := range words {
+		for len(char) > 0 && strings.ContainsAny(char[:1], ".,!?,") {
+			if len(result) > 0 {
+				result[len(result)-1] += char[:1]
+			}
+			char = char[:1]
+		}
+		if char != " " {
+			result = append(result, char)
+		}
+	}
+	return strings.Join(result, " ")
 }
